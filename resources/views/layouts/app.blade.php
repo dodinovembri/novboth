5fab58a2f66536d872a0055cd73662a464d3736a
dodinovembri <dodinovembri32gmail.com>
2022-04-13 11:44:07 +0700

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="keywords" content="Novboth - IT Solutions and Services" />
+    <meta name="description" content="Novboth - IT Sollutions and Services" />
+    <meta name="author" content="https://www.novboth.com/" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <title>Novboth - IT Solutions and Services</title>
+
+    <!-- favicon icon -->
+    <link rel="shortcut icon" href="images/favicon.png" />
+    <!-- bootstrap -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+    <!-- animate -->
+    <link rel="stylesheet" type="text/css" href="css/animate.css" />
+    <!-- owl-carousel -->
+    <link rel="stylesheet" type="text/css" href="css/owl.carousel.css">
+    <!-- fontawesome -->
+    <link rel="stylesheet" type="text/css" href="css/font-awesome.css" />
+    <!-- themify -->
+    <link rel="stylesheet" type="text/css" href="css/themify-icons.css" />
+    <!-- flaticon -->
+    <link rel="stylesheet" type="text/css" href="css/flaticon.css" />
+
+    <!-- REVOLUTION LAYERS STYLES -->
+    <link rel="stylesheet" type="text/css" href="revolution/css/rs6.css">
+    <!-- prettyphoto -->
+    <link rel="stylesheet" type="text/css" href="css/prettyPhoto.css">
+    <!-- shortcodes -->
+    <link rel="stylesheet" type="text/css" href="css/shortcodes.css" />
+    <!-- main -->
+    <link rel="stylesheet" type="text/css" href="css/main.css" />
+    <!-- responsive -->
+    <link rel="stylesheet" type="text/css" href="css/responsive.css" />
+
+</head>
+
+<body>
+    @yield('content')
+
+    <!-- Javascript -->
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/tether.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.easing.js"></script>
+    <script src="js/jquery-waypoints.js"></script>
+    <script src="js/jquery-validate.js"></script>
+    <script src="js/owl.carousel.js"></script>
+    <script src="js/jquery.prettyPhoto.js"></script>
+    <script src="js/numinate.min6959.js?ver=4.9.3"></script>
+    <script src="js/lazysizes.min.js"></script>
+    <script src="js/main.js"></script>
+
+    <!-- Revolution Slider -->
+    <script src="revolution/js/revolution.tools.min.js"></script>
+    <script src="revolution/js/rs6.min.js"></script>
+    <script src="revolution/js/slider.js"></script>
+    <!-- Javascript end-->
+</body>
+</html>
